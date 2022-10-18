1d760f9a97eb47626d62a63ededca4abe5e7472b
Conway <yawnoc@pm.me>
2022-10-19 01:05:35 +0800

    Write Writ.implies

--- sfta.py
+++ sfta.py
@@ -9,7 +9,7 @@ This is free software with NO WARRANTY etc. etc., see LICENSE.
 
 class Writ:
     """
-    Static class providing methods for manipulating writs.
+    Static class for performing calculations with writs.
 
     A __writ__ is an encoding of a boolean term (a conjunction of events)
     by setting the nth bit if and only if the nth event is present as a factor.
@@ -20,20 +20,48 @@ class Writ:
         10011 (binary),
     which is 19.
 
-    Note that the writ 0 encodes an empty conjunction, which is TRUE.
+    Note that the writ 0 encodes an empty conjunction, which is True.
     """
 
     @staticmethod
     def conjunction(input_writs):
         """
-        Compute the conjunction (logical AND) of input writs.
+        Compute the AND (conjunction) of some input writs.
 
         Since a factor is present in a conjunction
         if and only if it is present in at least one of the inputs,
         the conjunction writ is the bitwise OR of the input writs.
+
+        For example:
+            00000 | 00001 = 00001  <-->  True . A = A
+            10011 | 00110 = 10111  <-->  ABE . BC = ABCE
         """
         conjunction_writ = 0
         for writ in input_writs:
             conjunction_writ |= writ
 
         return conjunction_writ
+
+    @staticmethod
+    def implies(test_writ, reference_writ):
+        """
+        Decide whether a test writ implies a reference writ.
+
+        Equivalent to deciding whether the term represented by the test writ
+        is a multiple of the term represented by the reference writ.
+        If so, the test term would be redundant in a disjunction (OR)
+        with the reference term, as per the absorption law.
+
+        The test writ will not imply the reference writ if and only if there is
+        some bit not set in the test writ that is set in the reference writ.
+        Hence we compute the bitwise AND between the test writ negative
+        and the reference writ, and then compare unto zero.
+
+        For example:
+             ~00100 & 00000 = 00000  <-->  C implies True
+             ~00011 & 00001 = 00000  <-->  AB implies A
+             ~11111 & 10011 = 00000  <-->  ABCDE implies ABE
+             ~10000 & 00100 = 00100  <-->  E does not imply C (due to C)
+             ~11001 & 00111 = 00110  <-->  ADE does not imply ABC (due to BC)
+        """
+        return reference_writ & ~test_writ == 0
--- test_sfta.py
+++ test_sfta.py
@@ -24,6 +24,13 @@ class TestSfta(unittest.TestCase):
         self.assertEqual(Writ.conjunction([0b100, 0b010, 0b001]), 0b111)
         self.assertEqual(Writ.conjunction([0b1111, 0b0000, 0b001]), 0b1111)
 
+    def test_bitty_implies(self):
+        self.assertTrue(Writ.implies(0b00100, 0b00000))
+        self.assertTrue(Writ.implies(0b00011, 0b00001))
+        self.assertTrue(Writ.implies(0b11111, 0b10011))
+        self.assertFalse(Writ.implies(0b10000, 0b00100))
+        self.assertFalse(Writ.implies(0b11001, 0b00111))
+
 
 if __name__ == '__main__':
     unittest.main()
