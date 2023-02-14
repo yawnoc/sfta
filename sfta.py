576752c2eb55feafd20e434a5a878f3a931ec90c
Conway <yawnoc@pm.me>
2023-02-14 11:17:59 +0800

    Fix importance division by zero

--- sfta.py
+++ sfta.py
@@ -803,7 +803,9 @@ class Gate:
             for event in events
         }
         self.importance_from_event_index = {
-            event_index: contribution_value / self.quantity_value
+            event_index:
+                None if self.quantity_value == 0
+                else contribution_value / self.quantity_value
             for event_index, contribution_value
             in self.contribution_value_from_event_index.items()
         }
