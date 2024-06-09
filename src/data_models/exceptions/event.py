91efa3d236c89231bd54d15b125d2db22555bca2
Conway <yawnoc@pm.me>
2024-06-09 18:05:18 +0800

    Fix src/data_models/exceptions/*.py whitespace

--- src/data_models/exceptions/event.py
+++ src/data_models/exceptions/event.py
@@ -1,25 +1,33 @@
 from .base import FaultTreeTextException
 
+
 class EventLabelAlreadySetException(FaultTreeTextException):
     pass
+
 
 class EventQuantityAlreadySetException(FaultTreeTextException):
     pass
 
+
 class EventCommentAlreadySetException(FaultTreeTextException):
     pass
+
 
 class EventBadFloatException(FaultTreeTextException):
     pass
 
+
 class EventBadProbabilityException(FaultTreeTextException):
     pass
+
 
 class EventBadRateException(FaultTreeTextException):
     pass
 
+
 class EventUnrecognisedKeyException(FaultTreeTextException):
     pass
+
 
 class EventQuantityNotSetException(FaultTreeTextException):
     pass
--- src/data_models/exceptions/fault_tree.py
+++ src/data_models/exceptions/fault_tree.py
@@ -1,25 +1,33 @@
 from .base import FaultTreeTextException
 
+
 class FtSmotheredObjectDeclarationException(FaultTreeTextException):
     pass
+
 
 class FtDanglingPropertySettingException(FaultTreeTextException):
     pass
 
+
 class FtDuplicateIdException(FaultTreeTextException):
     pass
+
 
 class FtBadIdException(FaultTreeTextException):
     pass
 
+
 class FtBadLineException(FaultTreeTextException):
     pass
+
 
 class FtTimeUnitAlreadySetException(FaultTreeTextException):
     pass
 
+
 class FtUnrecognisedKeyException(FaultTreeTextException):
     pass
+
 
 class FtCircularGateInputsException(FaultTreeTextException):
     pass
