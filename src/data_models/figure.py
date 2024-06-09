a0e8dc312fb3f2bd1e0ac2d667b2c471e7d9c883
Conway <yawnoc@pm.me>
2024-06-09 22:31:14 +0800

    Revert figure.py quote style and reformat

--- src/data_models/figure.py
+++ src/data_models/figure.py
@@ -13,7 +13,11 @@ class Figure:
         top_node.position_recursive()
 
         self.top_node = top_node
-        self.occurrence_ids = {implicated_id for implicated_id in top_node.implicated_ids if implicated_id != id_}
+        self.occurrence_ids = {
+            implicated_id
+            for implicated_id in top_node.implicated_ids
+            if implicated_id != id_
+        }
 
     def get_svg_content(self):
         top_node = self.top_node
@@ -23,36 +27,36 @@ class Figure:
         width = top_node.width + 2 * Figure.MARGIN
         height = top_node.height + 2 * Figure.MARGIN
 
-        xmlns = "http://www.w3.org/2000/svg"
+        xmlns = 'http://www.w3.org/2000/svg'
         font_size = Node.DEFAULT_FONT_SIZE
         elements = top_node.get_svg_elements_recursive()
 
         return (
             f'<?xml version="1.0" encoding="UTF-8"?>\n'
             f'<svg viewBox="{left} {top} {width} {height}" xmlns="{xmlns}">\n'
-            f"<style>\n"
-            f"circle, path, polygon, rect {{\n"
-            f"  fill: lightyellow;\n"
-            f"}}\n"
-            f"circle, path, polygon, polyline, rect {{\n"
-            f"  stroke: black;\n"
-            f"  stroke-width: 1.3;\n"
-            f"}}\n"
-            f"polyline {{\n"
-            f"  fill: none;\n"
-            f"}}\n"
-            f"text {{\n"
-            f"  dominant-baseline: middle;\n"
+            f'<style>\n'
+            f'circle, path, polygon, rect {{\n'
+            f'  fill: lightyellow;\n'
+            f'}}\n'
+            f'circle, path, polygon, polyline, rect {{\n'
+            f'  stroke: black;\n'
+            f'  stroke-width: 1.3;\n'
+            f'}}\n'
+            f'polyline {{\n'
+            f'  fill: none;\n'
+            f'}}\n'
+            f'text {{\n'
+            f'  dominant-baseline: middle;\n'
             f'  font-family: Consolas, Cousine, "Courier New", monospace;\n'
-            f"  font-size: {font_size}px;\n"
-            f"  text-anchor: middle;\n"
-            f"}}\n"
-            f"</style>\n"
-            f"{elements}\n"
-            f"</svg>\n"
+            f'  font-size: {font_size}px;\n'
+            f'  text-anchor: middle;\n'
+            f'}}\n'
+            f'</style>\n'
+            f'{elements}\n'
+            f'</svg>\n'
         )
 
     def write_svg(self, file_name):
         svg_content = self.get_svg_content()
-        with open(file_name, "w", encoding="utf-8") as file:
+        with open(file_name, 'w', encoding='utf-8') as file:
             file.write(svg_content)
