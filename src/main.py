eef5ecb92d037ded0d147bda66b3d06fa5939aaf
Conway <yawnoc@pm.me>
2024-06-09 23:14:16 +0800

    Revert main.py quote style

--- src/main.py
+++ src/main.py
@@ -10,22 +10,22 @@ from src.data_models.fault_tree import FaultTree
 from src.data_models.exceptions.base import FaultTreeTextException
 
 
-__version__ = "0.6.2"
-DESCRIPTION = "Perform a slow fault tree analysis."
+__version__ = '0.6.2'
+DESCRIPTION = 'Perform a slow fault tree analysis.'
 
 
 def parse_command_line_arguments():
     argument_parser = argparse.ArgumentParser(description=DESCRIPTION)
     argument_parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"{argument_parser.prog} version {__version__}",
+        '-v',
+        '--version',
+        action='version',
+        version=f'{argument_parser.prog} version {__version__}',
     )
     argument_parser.add_argument(
-        "fault_tree_text_file_name",
-        help="name of fault tree text file; " "output is written unto the directory `{ft.txt}.out/`",
-        metavar="ft.txt",
+        'fault_tree_text_file_name',
+        help='name of fault tree text file; output is written unto the directory `{ft.txt}.out/`',
+        metavar='ft.txt',
     )
     return argument_parser.parse_args()
 
@@ -36,16 +36,16 @@ def main():
 
     if os.path.isdir(text_file_name):
         print(
-            f"Error: `{text_file_name}` is a directory, not a file",
+            f'Error: `{text_file_name}` is a directory, not a file',
             file=sys.stderr,
         )
         sys.exit(1)
 
     try:
-        with open(text_file_name, "r", encoding="utf-8") as file:
+        with open(text_file_name, 'r', encoding='utf-8') as file:
             fault_tree_text = file.read()
     except FileNotFoundError:
-        print(f"Error: file `{text_file_name}` existeth not", file=sys.stderr)
+        print(f'Error: file `{text_file_name}` existeth not', file=sys.stderr)
         sys.exit(1)
 
     try:
@@ -56,20 +56,20 @@ def main():
         message = exception.message
 
         if line_number:
-            error_location_str = f"at line {line_number} "
+            error_location_str = f'at line {line_number} '
         else:
-            error_location_str = ""
+            error_location_str = ''
 
         print(
-            f"Error {error_location_str}in `{text_file_name}`:\n  {message}",
+            f'Error {error_location_str}in `{text_file_name}`:\n  {message}',
             file=sys.stderr,
         )
         sys.exit(1)
 
-    output_directory_name = f"{text_file_name}.out"
+    output_directory_name = f'{text_file_name}.out'
     create_directory_robust(output_directory_name)
     write_output_files(fault_tree, output_directory_name)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
