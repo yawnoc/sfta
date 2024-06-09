665f7ac95f4b15c2b3afec026ff20fe75d718f28
Conway <yawnoc@pm.me>
2024-06-09 22:48:58 +0800

    Revert table.py quote style

--- src/data_models/table.py
+++ src/data_models/table.py
@@ -8,7 +8,7 @@ class Table:
         self.rows = rows
 
     def write_tsv(self, file_name):
-        with open(file_name, "w", encoding="utf-8", newline="") as file:
-            writer = csv.writer(file, delimiter="\t", lineterminator=os.linesep)
+        with open(file_name, 'w', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file, delimiter='\t', lineterminator=os.linesep)
             writer.writerow(self.field_names)
             writer.writerows(self.rows)
