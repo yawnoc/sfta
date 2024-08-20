2f0c037a21c883d70a46475daeadf07bb8455448
Conway <yawnoc@pm.me>
2024-08-20 22:06:33 +0800

    Reformat cli.py (whitespace changes)

--- sfta/cli.py
+++ sfta/cli.py
@@ -34,7 +34,7 @@ class DeepRecurse:
 
 def parse_command_line_arguments():
     argument_parser = argparse.ArgumentParser(
-        description='Perform a slow fault tree analysis.'
+        description='Perform a slow fault tree analysis.',
     )
     argument_parser.add_argument(
         '-v', '--version',
@@ -43,10 +43,7 @@ def parse_command_line_arguments():
     )
     argument_parser.add_argument(
         'fault_tree_text_file_name',
-        help=(
-          'name of fault tree text file; '
-          'output is written unto the directory `{ft.txt}.out/`'
-        ),
+        help='name of fault tree text file; output is written unto the directory `{ft.txt}.out/`',
         metavar='ft.txt',
     )
 
@@ -56,8 +53,10 @@ def parse_command_line_arguments():
 def create_directory_robust(directory_name):
     if os.path.isfile(directory_name):
         os.remove(directory_name)
+
     if os.path.isdir(directory_name):
         shutil.rmtree(directory_name)
+
     os.mkdir(directory_name)
 
 
@@ -66,10 +65,7 @@ def main():
     text_file_name = parsed_arguments.fault_tree_text_file_name
 
     if os.path.isdir(text_file_name):
-        print(
-            f'Error: `{text_file_name}` is a directory, not a file',
-            file=sys.stderr,
-        )
+        print(f'Error: `{text_file_name}` is a directory, not a file', file=sys.stderr)
         sys.exit(1)
 
     try:
@@ -91,10 +87,7 @@ def main():
         else:
             error_location_str = ''
 
-        print(
-            f'Error {error_location_str}in `{text_file_name}`:\n  {message}',
-            file=sys.stderr,
-        )
+        print(f'Error {error_location_str}in `{text_file_name}`:\n  {message}', file=sys.stderr)
         sys.exit(1)
 
     events_table = fault_tree.get_events_table()
@@ -117,14 +110,16 @@ def main():
 
     events_table.write_tsv(f'{output_directory_name}/events.tsv')
     gates_table.write_tsv(f'{output_directory_name}/gates.tsv')
+
     for gate_id, cut_set_table in cut_set_table_from_gate_id.items():
         cut_set_table.write_tsv(f'{cut_sets_directory_name}/{gate_id}.tsv')
+
     for gate_id, contribution_table in contribution_table_from_gate_id.items():
-        contribution_table.write_tsv(
-            f'{contributions_directory_name}/{gate_id}.tsv'
-        )
+        contribution_table.write_tsv(f'{contributions_directory_name}/{gate_id}.tsv')
+
     for figure_id, figure in figure_from_id.items():
         figure.write_svg(f'{figures_directory_name}/{figure_id}.svg')
+
     figure_index.write_html(f'{figures_directory_name}/index.html')
 
 
