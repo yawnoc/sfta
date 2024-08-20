700c3c06f2278c4d4887bc487a5bfde99f926d6f
Conway <yawnoc@pm.me>
2024-08-21 00:57:25 +0800

    Change demo_scripting.py example to self-contained

--- demo_scripting.py
+++ demo_scripting.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
+import textwrap
+
 from sfta.core import FaultTree
 
 
 def main():
-    with open('demos/readme-example.txt', 'r', encoding='utf-8') as file:
-        fault_tree_text = file.read()
+    fault_tree_text = textwrap.dedent('''
+        Event: A
+        - rate: 0.9
+
+        Event: B
+        - probability: 0.7
+
+        Event: C
+        - rate: 1e-4
+
+        Gate: AB
+        - label: This be an AND gate.
+        - type: AND
+        - inputs: A, B
+
+        Gate: AB_C
+        - label: This be an OR gate.
+        - type: OR
+        - inputs: AB, C
+    ''')
 
     fault_tree = FaultTree(fault_tree_text)
+    print(fault_tree.gate_from_id['AB_C'].quantity_value)
+
+    print(end='\n\n')
 
     print('## Fault tree', end='\n\n')
     print(fault_tree, end='\n\n\n')
